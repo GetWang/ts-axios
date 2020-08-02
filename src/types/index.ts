ac5b34040722ca6546a7d1891e727b764ecfb1a3
GetWang <wang_dresimap@qq.com>
2020-08-02 11:24:36 +0800

    feat(src): init axios function
    init axios function/method type/AxiosRequestConfig

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'delete'
+  | 'DELETE'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  params?: any
+  data?: any
+}
