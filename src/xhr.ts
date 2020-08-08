d0b4b257ff05d082df7ae6ec68f9a727907a3d71
GetWang <wang_dresimap@qq.com>
2020-08-08 11:51:37 +0800

    feat(src dir): handle responseData and responseHeaders
    process original responseHeaders into a plainObject and try to parse responseData into a JSON object

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,5 +1,7 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
+import { parseHeaders } from '../helpers/headers'
+
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise(resolve => {
     const { url, method = 'get', data = null, headers, responseType } = config
@@ -16,7 +18,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (request.readyState !== 4) {
         return
       }
-      const responseHeaders = request.getAllResponseHeaders()
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
         data: responseData,
