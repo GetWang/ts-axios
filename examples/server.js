659f361d892ec1179a866679c9f999f922f3d9bb
GetWang <wang_dresimap@qq.com>
2020-08-09 18:22:08 +0800

    feat(src/types): response data support generic
    response data support generic, and then typescript could infer the response data's type.

--- examples/server.js
+++ examples/server.js
@@ -116,24 +116,30 @@ function registerExtendRouter() {
   router.options('/extend/options', function(req, res) {
     res.end()
   })
-
   router.delete('/extend/delete', function(req, res) {
     res.end()
   })
-
   router.head('/extend/head', function(req, res) {
     res.end()
   })
-
   router.post('/extend/post', function(req, res) {
     res.json(req.body)
   })
-
   router.put('/extend/put', function(req, res) {
     res.json(req.body)
   })
-
   router.patch('/extend/patch', function(req, res) {
     res.json(req.body)
+  })
+
+  router.get('/extend/user', function(req, res) {
+    res.json({
+      code: 0,
+      result: {
+        name: 'wang',
+        age: 25
+      },
+      message: 'success'
+    })
   })
 }
